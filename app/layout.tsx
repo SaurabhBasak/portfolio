8aa43eb82f64c56cb3a95e219c8a7968167c071f
Saurabh Basak <saurabhbasak1@gmail.com>
2025-06-19 13:14:41 -0400

    Added Vercel Analytics integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "../components/theme-provider";
+import { Analytics } from "@vercel/analytics/next" ;
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -35,6 +37,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <Analytics />
           </ThemeProvider>
       </body>
     </html>
